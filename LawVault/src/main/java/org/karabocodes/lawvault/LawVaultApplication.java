327c9cbbea51fa0149e52d1c91b10ad1494fa5de
karabocodes <145660355+karabocodes@users.noreply.github.com>
2024-04-05 00:18:28 +0200

    skeleton code

---
+++ LawVault/src/main/java/org/karabocodes/lawvault/LawVaultApplication.java
@@ -0,0 +1,13 @@
+package org.karabocodes.lawvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LawVaultApplication.class, args);
+    }
+
+}
---
+++ LawVault/src/test/java/org/karabocodes/lawvault/LawVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package org.karabocodes.lawvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawVaultApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
